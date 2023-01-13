33fbe60914e663a05d125d07c6e4ae3f330dbf39
Liberty Mugwara <libertymugwara@live.com>
2023-01-13 16:25:54 +0200

    initial commit

---
+++ types/mugstech-shopify/index.ts
@@ -0,0 +1,8 @@
+export type TProductBulkMutationFns =
+  | "bulkCreateProducts"
+  | "bulkUpdateProductsImages"
+  | "bulkAppendProductsImages"
+  | "bulkUpdateProductsTags";
+export type TProductBulkQueryFns =
+  | "getAllProductVariantIds"
+  | "getAllProductImages";
